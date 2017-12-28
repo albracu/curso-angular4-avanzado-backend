a89ec55d1e3182a8075a57aa25630f2efecee99a
albracu <yo@alfredobravocuero.co>
2017-12-27 22:06:07 -0500

    Cargar imagen de usuario por Api REST

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,7 @@
 
 // Cargar Modulos
 var bcrypt = require('bcrypt-nodejs');
+var fs = require('fs');
 
 // Cargar Modelos
 var User = require('../models/user');
@@ -141,10 +142,59 @@ function updateUser(req, res){
     });
 }
 
+// Metodo para Cargar imagen de usuario
+function uploadImage(req, res){
+    var userId = req.params.id;
+    var file_name = 'No subido..';
+
+    if(req.files){
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('/');
+        var file_name = file_split[2];
+
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'png' || file_ext == 'jpng' || file_ext == 'gif'){
+
+            if(userId != req.user.sub){
+                return res.status(500).send({ message: 'No tiene permisos para actualizar el usuario' });
+            }
+        
+            User.findByIdAndUpdate(userId, {image: file_name}, {new: true}, (err, userUpdated) => {
+                if(err){
+                    res.status(500).send({
+                        message: 'Error al actualizar usuario'
+                    });
+                }else{
+                    if (!userUpdated) {
+                        res.status(404).send({message: 'No se ha podido utilizar el usuario'});
+                    }else{
+                        res.status(200).send({ user: userUpdated, image: file_name });
+                    }
+                }
+            });
+
+        }else{
+            fs.unlink(file_path, (err) => {
+                if (err){
+                    res.status(200).send({ message: 'Extension no valida y fichero no borrado'});
+                }else{
+                    res.status(200).send({ message: 'Extension no valida'});
+                }
+            });
+        }
+
+    }else{
+        res.status(200).send({ message: 'No se ha subido archivos'});
+    }
+}
+
 // Exportar acciones
 module.exports = {
     pruebas,
     saveUser,
     login,
-    updateUser
+    updateUser,
+    uploadImage
 };
