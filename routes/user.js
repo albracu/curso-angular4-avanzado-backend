670ae5d97e35e4b526a01c0682e323c74402347c
albracu <yo@alfredobravocuero.co>
2017-12-27 22:06:07 -0500

    Middleware de Authenticacion y Actualizacion de Usuario

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,9 @@ var UserController = require('../controllers/user');
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
+var multipart = require('connect-multiparty');
+var md_upload = multipart({ uploadDir: './uploads/users' });
+
 // Declaracion de rutas Api REST
 api.get('/pruebas-del-controlador', md_auth.ensureAuth, UserController.pruebas);
 
@@ -14,5 +17,7 @@ api.post('/register', UserController.saveUser);
 api.post('/login', UserController.login);
 
 api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
+
+api.post('/upload-image-user/:id', [md_auth.ensureAuth, md_upload], UserController.uploadImage);
 
 module.exports = api;
