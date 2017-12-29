304461ee31aefbd816afb94c15551e8c3575982c
albracu <yo@alfredobravocuero.co>
2017-12-28 20:51:26 -0500

    Configuracion de cabeceras HTTP y acceso a CORS

--- app.js
+++ app.js
@@ -14,7 +14,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Autorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 
+    next();
+})
 
 // Definir rutas base
 app.use('/api', user_routes);
